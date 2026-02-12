c67f208215c24dc101568afde9534b9d4adab0ed
wilfredcarl <will.foreshew@gmail.com>
2026-02-12 11:33:27 -0500

    Update TOKEN environment variable in bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ from telegram.ext import (
 
 # ✅ Recommended: set TOKEN as an environment variable in Railway
 # Railway: Variables -> add TOKEN = your_token
-TOKEN = os.getenv("TOKEN", "PASTE_YOUR_TOKEN_HERE")
+TOKEN = os.getenv("TOKEN", "8590522532:AAES85F2lQfkJgO243HYRV56vEiNQMC0X8w
+")
 
 TZ = ZoneInfo("America/Montreal")
 
